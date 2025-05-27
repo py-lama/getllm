77e3e724fc7803e7ea7f27523b265ae336713714
tom-sapletta-com <tom@sapletta.com>
2025-05-28 01:24:22 +0200

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8'
 
 setup(
     name="pyllm",
-    version="0.1.7",
+    version="0.1.8",
     description="Python LLM operations service for the DevLama ecosystem",
     long_description=long_description,
     long_description_content_type="text/markdown",
